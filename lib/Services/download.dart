33272a4c0f3e6338df551d3b5fa28124324b978b
BADU\badus <badushaebrahim62@gmail.com>
2022-05-09 14:59:51 +0530

    test format

--- lib/Services/download.dart
+++ lib/Services/download.dart
@@ -189,74 +189,74 @@ class Download with ChangeNotifier {
                       },
                     ),
                     SingleChildScrollView(
-                     scrollDirection: Axis.horizontal,
-                      child:
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            primary:
-                                Theme.of(context).brightness == Brightness.dark
-                                    ? Colors.white
-                                    : Colors.grey[700],
-                          ),
-                          onPressed: () {
-                            lastDownloadId = data['id'].toString();
-                            Navigator.pop(context);
-                            rememberOption = 0;
-                          },
-                          child: Text(
-                            AppLocalizations.of(context)!.no,
-                            style: const TextStyle(color: Colors.white),
-                          ),
-                        ),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            primary:
-                                Theme.of(context).brightness == Brightness.dark
-                                    ? Colors.white
-                                    : Colors.grey[700],
-                          ),
-                          onPressed: () async {
-                            Navigator.pop(context);
-                            Hive.box('downloads').delete(data['id']);
-                            downloadSong(context, dlPath, filename, data);
-                            rememberOption = 1;
-                          },
-                          child: Text(AppLocalizations.of(context)!.yesReplace),
-                        ),
-                        const SizedBox(width: 5.0),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            primary: Colors.white,
-                            backgroundColor:
-                                Theme.of(context).colorScheme.secondary,
-                          ),
-                          onPressed: () async {
-                            Navigator.pop(context);
-                            while (await File('$dlPath/$filename').exists()) {
-                              filename =
-                                  filename.replaceAll('.m4a', ' (1).m4a');
-                            }
-                            rememberOption = 2;
-                            downloadSong(context, dlPath, filename, data);
-                          },
-                          child: Text(
-                            AppLocalizations.of(context)!.yes,
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.secondary ==
-                                      Colors.white
-                                  ? Colors.black
-                                  : null,
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          TextButton(
+                            style: TextButton.styleFrom(
+                              primary: Theme.of(context).brightness ==
+                                      Brightness.dark
+                                  ? Colors.white
+                                  : Colors.grey[700],
+                            ),
+                            onPressed: () {
+                              lastDownloadId = data['id'].toString();
+                              Navigator.pop(context);
+                              rememberOption = 0;
+                            },
+                            child: Text(
+                              AppLocalizations.of(context)!.no,
+                              style: const TextStyle(color: Colors.white),
                             ),
                           ),
-                        ),
-                        const SizedBox(
-                          width: 5,
-                        ),
-                      ],
-                    ),),
+                          TextButton(
+                            style: TextButton.styleFrom(
+                              primary: Theme.of(context).brightness ==
+                                      Brightness.dark
+                                  ? Colors.white
+                                  : Colors.grey[700],
+                            ),
+                            onPressed: () async {
+                              Navigator.pop(context);
+                              Hive.box('downloads').delete(data['id']);
+                              downloadSong(context, dlPath, filename, data);
+                              rememberOption = 1;
+                            },
+                            child:
+                                Text(AppLocalizations.of(context)!.yesReplace),
+                          ),
+                          const SizedBox(width: 5.0),
+                          TextButton(
+                            style: TextButton.styleFrom(
+                              primary: Colors.white,
+                              backgroundColor:
+                                  Theme.of(context).colorScheme.secondary,
+                            ),
+                            onPressed: () async {
+                              Navigator.pop(context);
+                              while (await File('$dlPath/$filename').exists()) {
+                                filename =
+                                    filename.replaceAll('.m4a', ' (1).m4a');
+                              }
+                              rememberOption = 2;
+                              downloadSong(context, dlPath, filename, data);
+                            },
+                            child: Text(
+                              AppLocalizations.of(context)!.yes,
+                              style: TextStyle(
+                                color:
+                                    Theme.of(context).colorScheme.secondary ==
+                                            Colors.white
+                                        ? Colors.black
+                                        : null,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ],
